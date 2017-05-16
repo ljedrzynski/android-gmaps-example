248d73943f2bd12a37e583ce0582bc5dc31d2c15
ljedrzynski <lukjedrzynski@gmail.com>
2017-05-16 17:22:32 +0200

    schema corrections

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516114144) do
-
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170516080342) do
 
   create_table "users", force: :cascade do |t|
     t.string "nick"
